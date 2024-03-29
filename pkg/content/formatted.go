46e5ef7f0652a59603b754e2ae0614dd71ed6b63
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-03-29 16:37:56 +0300

    add special chars escaped

--- pkg/content/formatted.go
+++ pkg/content/formatted.go
@@ -28,7 +28,9 @@ func contentHTML(in []byte, out *bytes.Buffer) error {
 			html.WithXHTML(),
 		),
 	)
-	if err := md.Convert(bytes.ReplaceAll(in, []byte("$"), []byte("\\$")), out); err != nil {
+	in = bytes.ReplaceAll(in, []byte("$"), []byte("\\$"))
+	in = bytes.ReplaceAll(in, []byte("\\\n"), []byte("\\\\\n"))
+	if err := md.Convert(in, out); err != nil {
 		return fmt.Errorf("generate HTML content failed, got: %w", err)
 	}
 	return nil
